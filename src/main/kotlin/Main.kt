9d1c452ed34f84f545c77555219941cf4dc3f847
Aleksandr Karkhunen <ajlvatanabe@gmail.com>
2025-09-30 15:47:47 +0300

    Реализованы аудио, видео и фото вложения

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,7 @@
+import kotlin.time.Duration
+
 data class Post(
+    val original: Post? = null,
     val id: Int = 0,
     val ownerId: Int = 0,
     val fromId: Int = 0,
@@ -10,7 +13,8 @@ data class Post(
     val postType: String = "post",
     val isFavorite: Boolean = false,
     val comments: Comments = Comments(),
-    val likes: Likes = Likes()
+    val likes: Likes = Likes(),
+    val attachments: List<Attachment>? = null
 )
 
 data class Comments(
@@ -26,6 +30,58 @@ data class Likes(
     val userLikes: Boolean = false,
     val canLike: Boolean = false,
     val canPublish: Boolean = false
+)
+
+interface Attachment {
+    val type: String
+}
+
+data class ImagePreview(
+    val height: Int,
+    val width: Int,
+    val url : String,
+    val withpadding: Int
+)
+
+data class PhotoAttachment(val photo: Photo): Attachment {
+    override val type = "photo"
+}
+
+data class Photo(
+    val id: Int,
+    val ownerId: Int,
+    val album_id: Int,
+    val text: String,
+    val date: Int,
+    val photo130: String,
+    val photo604: String,
+    val photo807: String
+)
+
+data class VidoAttachment(val video: Video): Attachment {
+    override val type = "video"
+}
+
+data class Video(
+    val id: Int,
+    val ownerId: Int,
+    val title: String,
+    val image: Array<ImagePreview>,
+)
+
+data class audioAttachment (val audio: Audio):Attachment{
+    override val type = "audio"
+}
+
+data class Audio(
+    val id: Int,
+    val ownerId: Int,
+    val artist: String,
+    val title: String,
+    val duration: Int,
+    val url: String,
+    val genreid: Int,
+    val date: Int
 )
 
 class WallService {
