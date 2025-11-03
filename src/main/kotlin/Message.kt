471e08c8e6cb3d6a2c0e274d346b92e5f62bbb97
Aleksandr Karkhunen <ajlvatanabe@gmail.com>
2025-11-03 10:27:44 +0300

    Реализованы секвенции

--- src/main/kotlin/Message.kt
+++ src/main/kotlin/Message.kt
@@ -1,8 +1,8 @@
 data class Message(
-    val id: Int, // ид сообщения
-    val senderId: Int, // ид отправителя
-    var text: String, //текст сообщения
-    var isRead: Boolean = false //Прочитано или нет
+    val id: Int,
+    val senderId: Int,
+    var text: String,
+    var isRead: Boolean = false
 )
 
 data class Chat(
@@ -14,27 +14,46 @@ class ChatService {
     private val chats = mutableMapOf<Int, Chat>()
     private var messageIdCounter = 0
 
-    fun getChatsCount(): Int = chats.values.count { chats -> chats.message.any { !it.isRead } }
+    fun getChatsCount(): Int =
+        chats.values.asSequence()
+            .filter { chat -> chat.message.any { !it.isRead } }
+            .count()
+
     fun getChats(): List<Chat> = chats.values.toList()
-    fun getLastMessage(): List<String> = chats.values.map { chat -> chat.message.lastOrNull()?.text ?: "Нет Сообщения" }
+
+    fun getLastMessage(): List<String> =
+        chats.values.asSequence()
+            .map { chat -> chat.message.lastOrNull()?.text ?: "Нет Сообщения" }
+            .toList()
+
     fun getMessages(userId: Int, count: Int): List<Message> {
         val chat = chats[userId] ?: throw IllegalArgumentException("Чат не найден")
-        val messages = chat.message.takeLast(count)
-        messages.forEach { it.isRead = true }
-        return messages
+        val messages = chat.message
+        val start = (messages.size - count).coerceAtLeast(0)
+
+        return messages.asSequence()
+            .drop(start)
+            .onEach { it.isRead = true }
+            .toList()
     }
+
     fun sendMessage(toUserId: Int, fromUserId: Int, text: String) {
-        val chat = chats.getOrPut(toUserId) {Chat(toUserId)}
-        chat.message += Message(++messageIdCounter, fromUserId,text)
+        val chat = chats.getOrPut(toUserId) { Chat(toUserId) }
+        chat.message += Message(++messageIdCounter, fromUserId, text)
     }
+
     fun deleteMessage(userId: Int, messageId: Int) {
         val chat = chats[userId] ?: throw IllegalArgumentException("Чат не найден")
         chat.message.removeIf { it.id == messageId }
     }
+
     fun deleteChat(userId: Int) {
         chats.remove(userId)
     }
 }
 
-fun List<Message>.unreadCount(): Int = count {!it.isRead}
-fun List<Chat>.withUnread(): List<Chat> = filter { it.message.unreadCount() > 0 }
+fun List<Message>.unreadCount(): Int =
+    asSequence().count { !it.isRead }
+
+fun List<Chat>.withUnread(): List<Chat> =
+    asSequence().filter { it.message.unreadCount() > 0 }.toList()
